e48b5247bd7804b1ab721d5d20493e4776290460
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2017-09-08 13:18:46 -0300

    Adicionado recursos para emissão de faturas

---
+++ lib/vindi/rest/invoice.rb
@@ -0,0 +1,50 @@
+module Vindi
+  module Rest
+
+    # Methods for the invoices API
+    # @see https://vindi.github.io/api-docs/dist/#!/invoices
+    module Invoice
+
+      # List invoice for the authenticate user
+      # @return [Array<Hash>] A list of invoices for a merchant.
+      # @example Get all invoices from merchant vindi
+      def list_invoices(options = {})
+        get('invoices', options)[:invoices]
+      end
+
+      # Get a single invoice from a merchant
+      #
+      # @param invoice_id [Integer] ID of the invoice
+      # @return [Hash] The invoice you requested, if it exists
+      # @see https://vindi.github.io/api-docs/dist/#!/invoices/GET_version_invoices_id_format
+      # @example Get invoice #108 from vindi
+      #   client.invoice(108)
+      def invoice(invoice_id, options = {})
+        get("invoices/#{invoice_id}", options)[:invoice]
+      end
+
+      # Create a invoice for a merchant vindi
+      #
+      # @option options [Hash] :options invoice attributes
+      # @see https://vindi.github.io/api-docs/dist/#!/invoices/POST_version_invoices_format
+      # @return [Hash] The invoice created
+      # @example Create a invoice for a merchant vindi
+      #   client.create_invoice({ bill_id: 71, amount: 1 })
+      def create_invoice(options = {})
+        post('invoices', options)[:invoice]
+      end
+
+      # Delete a invoice from merchant vindi
+      #
+      # @params invoice_id [Integer] ID of the invoice
+      # @option options [Hash] :options invoice attributes
+      #
+      # @see https://vindi.github.io/api-docs/dist/#!/invoices/DELETE_version_invoices_id_format
+      # @example Delete invoice #108
+      #   client.delete_invoice(108)
+      def delete_invoice(invoice_id, options = {})
+        delete("invoices/#{invoice_id}", options)[:invoice]
+      end
+    end
+  end
+end
---
+++ spec/vindi/rest/invoice_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+RSpec.describe Vindi::Client::Invoice do
+  let(:client) { basic_auth_client }
+
+  describe 'list_invoices' do
+    it 'returns a collection of invoices' do
+      VCR.use_cassette("rest/invoices/list_invoices") do
+        list_invoices_response = client.list_invoices
+        assert_requested :get, vindi_url("invoices")
+        expect(list_invoices_response).to be_kind_of(Array)
+      end
+    end
+  end
+
+  describe 'invoice' do
+    it 'returns a invoice from vindi' do
+      VCR.use_cassette("rest/invoices/invoice") do
+        invoice_response = client.invoice(108)
+        assert_requested :get, vindi_url("invoices/108")
+        expect(invoice_response).to be_kind_of(Hash)
+      end
+    end
+  end
+
+  describe 'create_invoice' do
+    it 'returns the newly created invoice' do
+      VCR.use_cassette("rest/invoices/create_invoice") do
+        invoice_attributes = { bill_id: 71, amount: 1 }
+
+        invoice_response = client.create_invoice(invoice_attributes)
+        assert_requested :post, vindi_url("invoices")
+        expect(invoice_response[:amount]).to eq('1.0')
+      end
+    end
+  end
+
+  describe 'delete_invoice' do
+    it 'returns the deleted invoice' do
+      VCR.use_cassette("rest/invoices/delete_invoice") do
+        invoice_response = client.delete_invoice(108)
+        assert_requested :delete, vindi_url("invoices/108")
+        expect(invoice_response[:status]).to eq('canceled')
+      end
+    end
+  end
+end
